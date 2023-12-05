c1d00c81f2f3f65b27c6882e2e85fa57ffbe279a
pradnyap105 <133671417+pradnyap105@users.noreply.github.com>
2023-12-05 15:27:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
-import ParentComponent from './Components/Props_parent';
-import About from './Components/about';
+
+
+
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
 <Navbar title="WordCounter"/>
 <div className='container my-3'> 
 <TextForm  heading="Heading for Analysis"/>
-{/* <About /> */}
+
 </div>
 
 
@@ -21,21 +21,3 @@ function App() {
 
 export default App;
 
-
-// function App()
-//{
- 
-//     let data = shree ;
-//    function apple(){
-//     alert ('function Call')
-//    }
-//    return(
-  
-//     <div classNameName='App'>
-//       <h1>{data}</h1>
-//       <button onClick={apple}> click me</button>
-//     </div>
-//    )
-// }
-
-// export default App;
