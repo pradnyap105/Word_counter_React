3024edcb16fa42b16adc9234f0d4714846336cf0
pradnyap105 <133671417+pradnyap105@users.noreply.github.com>
2023-12-05 15:28:58 +0530

    Update Props_parent.js

--- src/Components/Props_parent.js
+++ src/Components/Props_parent.js
@@ -1,13 +1,3 @@
 // ParentComponent.jsx
 
-import React from 'react';
-import ChildComponent from './Props_child';
 
-const ParentComponent = () => {
-  // Data to be sent to the child component
-  const dataForChild = 'Hello from Parent!';
-
-  // Passing data to the child component using props
-  return <ChildComponent message={dataForChild} />;
-};
-export default ParentComponent;
